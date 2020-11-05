aa13f7b13aa41880de6d58d0372b680793d28ad2
SeaBass917 <sthiem@email.arizona.edu>
2020-11-05 01:45:16 -0700

    Added project modal box framework

---
+++ scripts/index.js
@@ -0,0 +1,59 @@
+
+// Main
+document.addEventListener("DOMContentLoaded", function() {
+
+    // ----------------------------------------------
+    // Set up the listener for each project modal
+    // ----------------------------------------------
+
+    // Dynamically determine what projects are in the projects section
+    let projectCards = document.getElementById("projects").getElementsByClassName("cardbox")[0].children
+    let projectNames = []
+    for (let projectCard of projectCards){
+        projectNames.push(projectCard.id)
+
+    }
+
+    // Set up the button listeners on each modal
+    for(let projectName of projectNames){
+
+        // Each project must have:
+        //    a modal popup, the button that opens it, and the button the closes it
+        let modal = document.getElementById(`${projectName}Modal`)
+        if(modal){
+            let btn = document.getElementById(`${projectName}`)
+            if(btn){
+                let btnClose = document.getElementById(`${projectName}BtnClose`)
+                if(btnClose){
+
+                    // When the user clicks on the button, open the modal
+                    btn.onclick = function() {
+                        modal.style.display = "block";
+                    }
+                    
+                    // When the user clicks on (x), close the modal
+                    btnClose.onclick = function() {
+                        modal.style.display = "none";
+                    }
+                    
+                    // When the user clicks anywhere outside of the modal, close it
+                    // NOTE: this technically means clicking on the modal, the stuff inside is "contents"
+                    window.onclick = function(event) {
+                        if (event.target.className == "modal") {
+                            event.target.style.display = "none";
+                        }
+                    }
+                }
+                else{
+                    console.error(`No closing button for ${projectName} modal in DOM.`);
+                }
+            }
+            else{
+                console.error(`No opening button for ${projectName} modal in DOM.`);
+            }
+        }
+        else{
+            console.error(`No modal div for ${projectName} in DOM.`);
+        }
+    }
+})
