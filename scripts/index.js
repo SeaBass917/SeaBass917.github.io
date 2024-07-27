837f1ff2fcd901875a0202245b390cff5321762a
SeaBass917 <sthiem@email.arizona.edu>
2024-07-26 22:53:33 -0400

    Fixed modal click issues; added new font

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,22 @@
+/**
+ * Hide a modal.
+ * @param {HTMLElement} modal The modal to hide.
+ */
+function hideModal(modal){
+    modal.style.display = "none"
+    modal.classList.remove("showModal")
+    modal.classList.add("hideModal")
+}
+
+/**
+ * Show a modal.
+ * @param {HTMLElement} modal The modal to show.
+ */
+function showModal(modal){
+    modal.style.display = "block"
+    modal.classList.add("showModal")
+    modal.classList.remove("hideModal")
+}
 
 // Main
 document.addEventListener("DOMContentLoaded", function() {
@@ -8,82 +27,85 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Dynamically determine what projects are in the projects section
     let projectCards = document.getElementById("projects").getElementsByClassName("cardbox")[0].children;
-    let projectNames = [];
+    let modalNames = [];
     for (let projectCard of projectCards){
-        projectNames.push(projectCard.id);
+        modalNames.push(projectCard.id);
     }
     // Dynamically determine what projects are in the publications section
     let pubCards = document.getElementById("publications").getElementsByClassName("cardbox")[0].children;
     for (let pubCard of pubCards){
-        projectNames.push(pubCard.id);
+        modalNames.push(pubCard.id);
     }
     // Dynamically determine what projects are in the publications section
     let presiCards = document.getElementById("presentations").getElementsByClassName("cardbox")[0].children;
     for (let presiCard of presiCards){
-        projectNames.push(presiCard.id);
+        modalNames.push(presiCard.id);
     }
 
     // Set up the button listeners on each modal
-    for(let projectName of projectNames){
+    for(let projectName of modalNames){
 
-        // Each project must have:
+        // Gather deps
+        // Each modal view must have:
         //    a modal popup, the button that opens it, and the button the closes it
-        let modal = document.getElementById(`${projectName}Modal`);
-        if(modal){
-            let btn = document.getElementById(`${projectName}`)
-            if(btn){
-                let btnClose = document.getElementById(`${projectName}BtnClose`)
-                if(btnClose){
+        let modalElement = document.getElementById(`${projectName}Modal`);
+        let cardElement = document.getElementById(`${projectName}`)
+        let modalXButtonElement = document.getElementById(`${projectName}BtnClose`)
 
-                    // When the user clicks on the button, open the modal
-                    btn.onclick = function() {
-                        let modal = document.getElementById(`${this.id}Modal`)
-                        modal.style.display = "block"
-                        modal.classList.add("showModal")
-                        modal.classList.remove("hideModal")
-
-                        // Load the videos
-                        // Play the first one
-                        const video_eles = modal.getElementsByTagName("video");
-                        for(let video_ele of video_eles){
-                            video_ele.preload = "auto";
-                        }
-                        if(video_eles.length) video_eles[0].play();
-                    }
-                    
-                    // When the user clicks on (x), close the modal
-                    btnClose.onclick = function() {
-                        let modal = document.getElementById(`${this.id.substring(0, this.id.length-8)}Modal`)
-                        modal.style.display = "none"
-                        modal.classList.remove("showModal")
-                        modal.classList.add("hideModal")
-                    }
-                    
-                    // When the user clicks anywhere outside of the modal, close it
-                    // NOTE: this technically means clicking on the modal, the stuff inside is "contents"
-                    window.onclick = function(event) {
-                        if (event.target.className.includes("modal")) {
-                            let openModals = document.getElementsByClassName('showModal');
-                            if(openModals){
-                                for(let modal of openModals){
-                                    modal.style.display = "none"
-                                    modal.classList.remove("showModal")
-                                    modal.classList.add("hideModal")
-                                }
-                            }
-                        }
-                    }
-                }
-                else{
-                    console.error(`No closing button for ${projectName} modal in DOM.`)
-                }
-            }
-            else{
-                console.error(`No opening button for ${projectName} modal in DOM.`)
-            }
-        }
-        else{
+        if(modalElement === null){
             console.error(`No modal div for ${projectName} in DOM.`)
+            continue;
+        }
+        if(cardElement === null){
+            console.error(`No opening button for ${projectName} modal in DOM.`)
+            continue;
+        }
+        if(modalXButtonElement === null){
+            console.error(`No closing button for ${projectName} modal in DOM.`)
+            continue;
+        }
+
+        // When the user clicks on the button, open the modal
+        cardElement.onclick = function() {
+
+            let modal = document.getElementById(`${this.id}Modal`);
+            showModal(modal);
+
+            // Load the videos
+            // Play the first one
+            const video_eles = modal.getElementsByTagName("video");
+            for(let video_ele of video_eles){
+                video_ele.preload = "auto";
+            }
+            if(video_eles.length) video_eles[0].play();
+        }
+        
+        // When the user clicks on (x), close the modal
+        modalXButtonElement.onclick = function() {
+            let modal = document.getElementById(`${this.id.substring(0, this.id.length-8)}Modal`);
+            hideModal(modal);
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        // NOTE: The modal is the whole screen, so we need to check if the click happens outside the bounds of the modal
+        modalElement.onclick = function(event) {
+            // get the location of this modal's `modal-content` child element
+            let modalRect = this.getElementsByClassName("modal-content")[0].getBoundingClientRect();
+
+            // Add 20 pixels to the to all bounds to account for the padding
+            modalRect.left -= 20;
+            modalRect.right += 20;
+            modalRect.top -= 20;
+            modalRect.bottom += 20;
+
+            // get the location of the click
+            let clickX = event.clientX;
+            let clickY = event.clientY;
+
+            // if the click is outside the modal, close it
+            if(clickX < modalRect.left || clickX > modalRect.right || clickY < modalRect.top || clickY > modalRect.bottom){
+                hideModal(this);
+            }
         }
     }
 })
